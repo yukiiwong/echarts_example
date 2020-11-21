e82d5e4f4777e4bd1e13c8cc40bc91ecd5bca614
yukiiwong <542735278@qq.com>
2020-11-21 19:43:38 +0800

    test for html and js

---
+++ test_js.js
@@ -0,0 +1,7 @@
+function displayDate(){
+    document.getElementById("demo").innerHTML=Date();
+}
+var x=document.getElementById("main");
+var y=x.getElementsByTagName("p");
+console.log("11111111")
+document.write('id="main"元素中的第一个段落为：' + y[0].innerHTML);
